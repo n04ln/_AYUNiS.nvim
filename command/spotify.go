5047a3be789b36a8979860e0b0e188289c45686a
NoahOrberg <noah.orberg.9999@gmail.com>
2018-04-15 18:06:38 +0900

    u

--- command/spotify.go
+++ command/spotify.go
@@ -1,6 +1,11 @@
 package command
 
-import "github.com/neovim/go-client/nvim"
+import (
+	"os/exec"
+
+	"github.com/NoahOrberg/nimvle.nvim/nimvle"
+	"github.com/neovim/go-client/nvim"
+)
 
 type Spotify struct {
 	NowPlaying string
@@ -17,6 +22,26 @@ func NewSpotify() *Spotify {
 	return s
 }
 
+func (s *Spotify) Init(v *nvim.Nvim, args []string) error {
+	nimvle := nimvle.New(v, "AYUNiS.nvim")
+	pollingNowPlaying(nimvle)
+	return nil
+}
+
 func (s *Spotify) GetNowPlaying(v *nvim.Nvim, args []string) (string, error) {
 	return s.NowPlaying, nil
+}
+
+func (s *Spotify) pollingNowPlaying(nimvle *nimvle.Nimve) {
+	go func() {
+		for i := uint64(0); ; i++ {
+			out, err := exec.Command("/usr/bin/osascript", "spotify_util/now_playing.applescript").Output()
+			if err != nil {
+				nimvle.Log(err.Error())
+			}
+
+			s.NowPlaying = string(out)
+		}
+	}()
+
 }
